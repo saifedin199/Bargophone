a94833d2f9d305e1cc83a6ed442970ffc404be05
saifedin199 <66060625+saifedin199@users.noreply.github.com>
2026-04-01 21:07:22 +0100

    Create phones.js

---
+++ phones.js
@@ -0,0 +1,43 @@
+let phones=[
+
+{
+
+name:"Vivo X100 Pro",
+
+brand:"Vivo",
+
+processor:"Dimensity 9300",
+
+ram:"16GB",
+
+storage:"512GB",
+
+battery:"5400mAh",
+
+camera:"50MP",
+
+img:"https://fdn2.gsmarena.com/vv/pics/vivo/vivo-x100-pro-1.jpg"
+
+},
+
+{
+
+name:"Samsung S24 Ultra",
+
+brand:"Samsung",
+
+processor:"Snapdragon 8 Gen 3",
+
+ram:"12GB",
+
+storage:"256GB",
+
+battery:"5000mAh",
+
+camera:"200MP",
+
+img:"https://fdn2.gsmarena.com/vv/pics/samsung/galaxy-s24-ultra-1.jpg"
+
+}
+
+]
